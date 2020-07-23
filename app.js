a2e10ae84f0a6c6f5e33d1d3c1de437238392996
GonzalezL13 <luisgonzalez1331@icloud.com>
2020-07-22 19:36:05 -0700

    beginning of inquirer

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
+const inquirer = require("inquirer");
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
+
+//const fs = require("fs");
+//const generatePage = require("./src/page-template.js");
+
+//const profileDataArgs = process.argv.slice(2);
+
+//const [name, github] = profileDataArgs;
 //const name = profileDataArgs[0]; same as top line
 //const github = profileDataArgs[1]; 
 
@@ -16,8 +29,8 @@ const [name, github] = profileDataArgs;
 //};
 // printProfileData(profileDataArgs);
 
-fs.writeFile("./index.html", generatePage(name, github), err => {
-    if (err) throw new Error(err);
+//fs.writeFile("./index.html", generatePage(name, github), err => {
+    //if (err) throw new Error(err);
 
-    console.log("Portfolio complete! Check out index.html to see the output!");
-});
+    //console.log("Portfolio complete! Check out index.html to see the output!");
+//});
